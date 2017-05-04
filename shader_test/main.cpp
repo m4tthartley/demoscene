f0a4fb1ded7fd8082b2e4b9a600c5ea53e72e1b2
Matt Hartley <m4tthartley@gmail.com>
2017-05-04 01:08:59 +0100

    Playing with shader ray marching and fun stuff

---
+++ shader_test/main.cpp
@@ -0,0 +1,49 @@
+
+#include "w:/libs/rain.c"
+#include <GL/glu.h>
+#include "w:/libs/gfx.c"
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	Rain rain = {};
+	rain.window_width = 1920;
+	rain.window_height = 1080;
+	rain_init(&rain);
+	SetWindowLongPtr(_win32.window, GWL_STYLE, WS_OVERLAPPED|WS_POPUP);
+	SetWindowPos(_win32.window, NULL, 0, 0, rain.window_width, rain.window_height, SWP_SHOWWINDOW);
+	load_opengl_extensions();
+	Shader shader = shader_from_file("shader.glsl", SHADER_VERTEX|SHADER_PIXEL);
+	while (!rain.quit) {
+		rain_update(&rain);
+
+		no_shader();
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		float2 verts[] = {
+			-1.0, -1.0,
+			1.0, -1.0,
+			1.0, 1.0,
+			-1.0, -1.0,
+			1.0, 1.0,
+			-1.0, 1.0,
+		};
+
+		use_shader(&shader);
+		int u_time = glGetUniformLocation(shader.gl_program, "time");
+		glUniform1f(u_time, rain.time_s);
+		int u_pos = glGetAttribLocation(shader.gl_program, "vertex_position");
+		glEnableVertexAttribArray(u_pos);
+		glVertexAttribPointer(u_pos, 2, GL_FLOAT, GL_FALSE, 0, verts);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+	}
+
+	float a = 5.0;
+	float b = a * 0.5;
+
+	float c = 5.0f;
+	float d = c * 0.5f;
+
+	printf("%f %f \n", b, d);
+}
